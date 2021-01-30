08efca9ad4c5eb63cf886efdf6dd73ca95acb3b0
Kostas Vlachos <kvlachos.geo@gmail.com>
2021-01-30 14:00:58 +0200

    Add and adjust test for constructor and addMoney method

--- test_mods.py
+++ test_mods.py
@@ -6,3 +6,9 @@ class TestWOFPlayer(unittest.TestCase):
     def test_init(self):
         p = WOFPlayer('Kostas')
         self.assertEqual((p.name, p.prizeMoney, p.prizes), ('Kostas', 0, []))
+    
+    def test_addMoney_1(self):
+        p = WOFPlayer('Kostas')
+        p.addMoney(10)
+        expected = 10
+        self.assertEqual(p.prizeMoney, expected)
