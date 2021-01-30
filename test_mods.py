6b735f450eb246678119062f75ea835101e491ff
Kostas Vlachos <kvlachos.geo@gmail.com>
2021-01-30 14:47:26 +0200

    Add test for addMoney method

--- test_mods.py
+++ test_mods.py
@@ -12,3 +12,8 @@ class TestWOFPlayer(unittest.TestCase):
         p.addMoney(10)
         expected = 10
         self.assertEqual(p.prizeMoney, expected)
+    def test_addMoney_2(self):
+        p = WOFPlayer('Kostas')
+        p.addMoney('g')
+        expected = 0
+        self.assertEqual(p.prizeMoney, expected)
